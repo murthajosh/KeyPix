4bd0ba16ff858cf9864602a2098c701fa1e60f06
Josh Murtha <murthajosh@gmail.com>
2021-03-12 15:31:15 -0600

    extend range working!
    range can be extended and trimmed on the treble side.
    
    Gotta do the same on the bass side with 'prepend'

--- app.js
+++ app.js
@@ -16,4 +16,40 @@ $( document ).ready(function() {
           this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
         });
     });
+
+    var ceChunk = `
+    <div class="whiteKey C">
+        <p class="whiteNoteName">C</p>
+    </div>
+
+    <div class="whiteKey D">
+        <span class="blackKey Db">
+            <p class="blackNoteName">D<sup>b</sup></p>
+        </span>
+        <p class="whiteNoteName">D</p>
+    </div>
+
+    <div class="whiteKey E">
+        <span class="blackKey Eb">
+            <p class="blackNoteName">E<sup>b</sup></p>
+        </span>
+        <p class="whiteNoteName">E</p>
+    </div>
+    `;
+
+    $("#treble-extend").click(function() {
+      $(".keyBoard").append(ceChunk);
+    });
+
+    $("#treble-trim").click(function() {
+      $(".keyBoard").children().last().remove();
+    });
+
+    jQuery(document).on('click', '.whiteKey,.natural-orange, .natural-blue', function (event) {
+      var classes = ['natural-orange','natural-blue','whiteKey'];
+      $(this).each(function(){
+        this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
+      });
+  });
+
 });
